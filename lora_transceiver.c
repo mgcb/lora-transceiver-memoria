fb0515bdbb2d843e160fc8fe24357d07c24bc8c9
mgcb <melanie.corletto@sansano.usm.cl>
2022-10-17 18:29:26 -0300

    more comments

--- lora_transceiver.c
+++ lora_transceiver.c
@@ -828,17 +828,3 @@ int main (int argc, char *argv[]) {
     }
     exit(0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
