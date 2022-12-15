86e1157793c811fdb46184f942e68c1fbc884c9a
mgcb <melanie.corletto@sansano.usm.cl>
2022-12-14 23:21:24 -0300

    test

--- extra/frameToText.py
+++ extra/frameToText.py
@@ -4,7 +4,7 @@ from pytesseract import pytesseract
 #Define path to tessaract.exe
 path_to_tesseract = r'/usr/bin/tesseract'
 
-#Define path to image
+#Define path to image test
 path_to_image = './frame293.jpg'
 
 #Point tessaract_cmd to tessaract.exe
