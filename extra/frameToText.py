017f5da0dcc89cf03c5cce2bad3d5801e4fd5f9e
mgcb <melanie.corletto@sansano.usm.cl>
2022-12-20 22:21:23 -0300

    write on file

--- extra/frameToText.py
+++ extra/frameToText.py
@@ -10,11 +10,13 @@ pytesseract.tesseract_cmd = path_to_tesseract
 
 #Define path to image
 folder = r'./'
+f = open("./results", "w")
+
 
 for root, dirs, file_names in os.walk(folder):
     for file_name in file_names:
         img_cv = cv2.imread(folder + file_name)
 
         img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
-        print(pytesseract.image_to_string(img_rgb))
+        f.write(pytesseract.image_to_string(img_rgb))
 
