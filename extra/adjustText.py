46e0cbc34a215757729bcd2a2ce9171b6269df28
mgcb <melanie.corletto@sansano.usm.cl>
2023-06-27 19:59:59 -0400

    Deleting Extra codes

--- extra/adjustText.py
+++
@@ -1,9 +0,0 @@
-count = 0
-numbers = []
-
-f = open("./10seg-1-clean.txt", "w")
-with open('texto.txt', 'r') as file:
-	for line in file:
-		if '0.' in line:
-			f.write(str(count)+"-"+str(line))
-		count += 1
